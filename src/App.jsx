063b256abd010381837241f5d545f8807511afd9
Arafat Patwary <farabipsk@gmail.com>
2024-11-23 20:15:43 +0600

    Add simple Tailwind template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import HomePage from "./TestCode/HomePage";
 
 function App() {
   return (
     <>
+      <HomePage />
       <h1>React Redux Project</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
